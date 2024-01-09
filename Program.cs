d78c602e3f433968e15ca5a797fe72ca712feb94
Alyff Antonio <alyffantonio@gmail.com>
2024-01-09 12:37:50 -0300

    Conclusãogit add poo-celular.slngit add poo-celular.sln

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.Reflection;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"123456", modelo: "Tijolão", imei:"121212", memoria: 64 );
+nokia.Ligar();
+nokia.InstalarAplicativo("WildRift");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero:"123321", modelo:"15-PRO", imei:"445566", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
